a60f3654692d2e0410cc91367fc5a8cc9c7bbb4e
Viacheslav Trofymenko <vtrofyme@c2r1s6.42madrid.com>
2025-04-30 15:15:04 +0200

    Added define of Buf size in a header

--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+
+# endif
